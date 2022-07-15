965be1db9bde6dd176501ab6ebe9a36ca3e8d98c
Aryan Tadepalli <aryantadepalli50@yahoo.com>
2022-07-15 12:10:55 -0400

    remake the whole sabra client in raw css
    this is to make it to my own specifications instead of material design's

---
+++ src/components/Marked.vue
@@ -0,0 +1,40 @@
+<template>
+  <span v-html="parseMarkdown()"></span>
+</template>
+
+<script>
+
+  import {marked} from 'marked';
+  export default {
+    name: 'HelloWorld',
+
+    props: {
+      markdown: { required: true, type: String },
+    },
+
+
+    methods:{
+      parseMarkdown(){
+        marked.use({
+          pedantic: false,
+          gfm: false,
+          breaks: false,
+          sanitize: false,
+          smartLists: true,
+          smartypants: false,
+          xhtml: true
+        });
+        return marked.parseInline(this.markdown)
+
+      },
+    },
+  }
+</script>
+
+<style scoped>
+  p{
+    margin:0; 
+    margin-left: 60px; 
+    white-space: pre-wrap;
+  }
+</style>
