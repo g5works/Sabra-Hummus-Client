c9292652a024dc7fac3f19cca5de1b1d78178d74
Aryan Tadepalli <aryantadepalli50@yahoo.com>
2022-07-15 19:38:24 -0400

    start redoing the ui in plain css instead of vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,343 @@
 <template>
-  <marked markdown=""></marked>
+  <div class="parent-container">
+    <div id="guildlist">
+    </div>
+    <div id="channellist">
+    </div>
+
+    
+
+    <div id="therest">
+      <div id="menubar">
+        <p style="font-size: 15pt; margin: 6px 0px; width: 150px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; border-right: solid 1px #9da8d1; display: inline-block">#channel-namessss</p><p style=" margin: 9px 0px; margin-left: 5px; width: calc(100% - 150px - 10px); white-space: nowrap; overflow: hidden; text-overflow: ellipsis; display: inline-block">#channel-namesssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
+      </div>
+      <div id="messages">
+        <div class="messagebody">
+          <div class="message-avatar" style="background-image: url('https://picsum.photos/200');"></div>
+          <div class="message-content">
+            <span class="character-title">iMacG5</span>
+            <p class="message-text">Hello there this is a message sampler</p>
+          </div>
+        </div>
+      </div>
+      <div id="sender">
+      </div>
+    </div>
+
+    <div id="memberlist">
+    </div>
+ 
+  </div>
 </template>
 
 <script>
-import Marked from './components/Marked.vue'
+// import Marked from './components/Marked.vue'
+// import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    Marked
-  }
+    // Marked
+  },
+
+  data: () => {
+    return {
+      slidepanel: true,
+      chatboxcontent: undefined,
+      chatboxsendicon: "",
+
+      guildlist: [],
+      guildchannels: [],
+      selectedguild: undefined,
+      selectedchannel: undefined,
+
+      guilddata: [],
+
+      channelposition: 0,
+
+      gatewaySocket: undefined,
+
+      token: "OTkxODMyNzY5ODgyMjU1Mzcx.YqkULw.xSNZ4-5fvYTq4mm9cMEh99ysT0t8Ncm5FcpFow", 
+      messages: [],
+      servername: "x",
+      serverid: undefined,
+      channelname: undefined,
+      channelid: undefined,
+      sequencing: null,
+      heartbeat: undefined,
+    }
+  },
+  // methods:{
+  //   eventtest(){
+  //     if (this.chatboxcontent != null){
+  //       alert(this.chatboxcontent)
+  //     }
+  //     else if (this.chatboxcontent != ""){
+  //       alert(this.chatboxcontent)
+  //     }
+  //     else{
+  //       alert("there is nothing to send to this event listener")
+  //     }
+
+  //   },
+
+  //   sendMessage(){
+  //     axios.post(`https://hummus.sys42.net/api/channels/909523692261834752/messages`, {
+  //       "content": `${this.chatboxcontent}`,
+  //       "tts": false
+  //     }, { headers: {"authorization" : `${this.token}`}})
+  //     this.chatboxcontent = ""
+  //   },
+
+  //   getAvatar(){
+  //     return "https://hummus-stg-cdn.sys42.net/avatars/994620142638624782/e3afcf9bfc60773169062ec83ce50e23.png"
+  //   },
+
+  //   getExtension(file){
+  //     var extension = file.split(".").pop();
+  //     return extension
+  //   },
+  //   async updateMsgs(){
+  //     var chats = await axios.get(`https://hummus.sys42.net/api/channels/909523692261834752/messages?limit=50`, { headers: {"authorization" : `${this.token}`}})
+  //     this.messages = chats.data.reverse()
+  //   },
+  //   async getGuildList(){
+  //     // var self = this
+  //     this.gatewaySocket.addEventListener("message", (event) => {
+        
+  //       var eventdata = JSON.parse(event.data)
+  //       // console.log(eventdata)
+  //       switch(eventdata.t){
+  //         case 'READY':
+              
+  //             this.guildlist = eventdata.d.guilds
+  //             console.log(eventdata.d.guilds)
+  //             console.log(eventdata.t)
+  //             console.log(this.guildlist)
+  //             this.selectedguild = this.guildlist[0]
+  //             this.guildchannels = this.selectedguild.channels.sort((a, b)=>(a.position - b.position))
+              
+  //             this.selectedchannel = this.guildchannels[0]
+  //             this.servername = this.selectedguild.name
+  //             this.serverid = this.selectedguild.id
+  //             axios.get(`https://hummus.sys42.net/api/channels/${this.selectedchannel.id}/messages?limit=50`, { headers: {"authorization" : `${this.token}`}}).then((response) => {this.messages = response.data.reverse()})
+  //             // axios.get(`https://hummus.sys42.net/api/guilds/${this.selectedguild}/roles`, { headers: {"authorization" : `${this.token}`}}).then((response) => {console.log(response)})
+  //             break;
+  //         case 'TYPING_START':
+  //           console.log(`${eventdata.d.member.nick} is typing`)
+  //           break;
+  //         case 'MESSAGE_CREATE':
+  //           console.log(`${eventdata.d.author.username} has sent a message`)
+  //           break;
+  //         case 'MESSAGE_UPDATE':
+  //           console.log(`${eventdata.d.author.username} has updated a message`)
+  //           break;
+  //         case 'PRESENCE_UPDATE':
+  //           console.log(`User ${eventdata.d.user.username} has come/gone ${eventdata.d.status}`)
+  //           break;
+  //       }
+        
+  //     })    
+      
+  //   },
+
+  //   changeGuild(index){
+  //     this.selectedguild = this.guildlist[index]
+  //     this.guildchannels = this.selectedguild.channels.sort((a, b)=>(a.position - b.position))
+      
+  //     console.log(this.guildchannels)
+  //     this.servername = this.selectedguild.name
+  //     this.serverid = this.selectedguild.id
+
+  //     // this.selectedchannel = this.guildchannels[selectedchannelindex]
+
+  //     // axios.get(`https://hummus.sys42.net/api/channels/${this.selectedchannel.id}/messages?limit=50`, { headers: {"authorization" : `${this.token}`}}).then((response) => {this.messages = response.data.reverse()})
+
+      
+  //   },
+
+  //   changeChannel(index){
+  //     this.selectedchannel = this.guildchannels[index]
+
+  //     axios.get(`https://hummus.sys42.net/api/channels/${this.selectedchannel.id}/messages?limit=50`, { headers: {"authorization" : `${this.token}`}}).then((response) => {this.messages = response.data.reverse()})
+  //   }
+
+  // },
+
+  // watch: {
+  //   chatboxcontent () {
+  //     if (this.chatboxcontent.length == 0 || this.chatboxcontent == null){
+  //       this.chatboxsendicon = null
+  //     }
+  //     else{
+  //       this.chatboxsendicon = "mdi-send"
+  //     }
+  //   }
+  // },
+
+  // async created() {
+  //   this.gatewaySocket = new WebSocket("wss://hummus-stg-gateway.sys42.net/?v=6&encoding=json")
+  //   var self = this
+
+
+  //   this.gatewaySocket.addEventListener("open", () => {
+  //       self.gatewaySocket.send(JSON.stringify({
+  //         op: 2,
+  //         d: {
+  //           token: this.token,
+  //           intents: 513,
+  //           properties: {
+  //             $os: "linux",
+  //             $browser: "disco",
+  //             $device: "disco"
+  //           }
+  //         }
+
+  //       }))
+  //     })
+  //   this.gatewaySocket.addEventListener('message', (event) => {
+  //       var eventdata = JSON.parse(event.data)
+  //       switch(eventdata.op){
+  //           case 10:
+  //               this.sequencing = eventdata.s
+  //               var heartbeat = eventdata.d.heartbeat_interval
+  //               console.log('beat')
+  //               self.gatewaySocket.send(JSON.stringify({
+  //                   op: 1,
+  //                   d: this.sequencing
+  //               }))
+  //               setInterval(() => {
+  //                   self.gatewaySocket.send(JSON.stringify({
+  //                       op: 1,
+  //                       d: this.sequencing
+  //                   }))
+  //               }, heartbeat)
+  //               break;
+  //       }
+  //     });
+    
+  //   console.log(await this.getGuildList())
+    
+  // },
+
+  // mounted(){
+
+  // }
 }
 </script>
 
 <style>
-  
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+
+  html, body{
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+
+  p , h1, h2, h3, h4, h5, h6, span{
+    font-family: 'Open Sans';
+    color: white;
+  }
+
+
+  #guildlist{
+    float: left;
+    height: 100vh;
+    width: 60px;
+    background-color: rgb(40, 40, 40);
+  }
+  #channellist{
+    float: left;
+    height: 100vh;
+    width: 250px;
+    background-color: rgb(50, 50, 50);
+  }
+
+  #memberlist{
+    float: right;
+    height: 100vh;
+    width: 220px;
+    
+    position: relative;
+    background-color: rgb(40, 40, 40);
+  }
+
+  /*#menubar{
+    position: absolute;
+    height: 40px;
+    background-color: #7289da;
+    width: calc(100% - 60px - 250px - 220px - 10px);
+    padding: 5px;
+  }*/
+
+  #therest{
+    overflow: auto;
+    float: left;
+    height: 100vh;
+    width: calc(100% - 60px - 250px - 220px);
+    background-color: rgb(75, 75, 75);
+  }
+
+  #menubar{
+    
+  }
+
+  #messages{
+    
+  }
+
+  #sender{
+    display: flex;
+    background-color: green;
+    height: 60px;
+    align-items: stretch;
+  }
+
+
+
+  .v-break{
+    border-left: black 5px solid;
+  }
+
+  /* .slide-members-enter-active, .slide-members-leave-active{
+    transition: all .5s;
+  }
+
+  .slide-members-enter, .slide-members-leave-to{
+    transform: translate(220px,0);
+  } */
+
+
+  .messagebody{
+    width: calc(100% - 10px);
+    min-height: 60px;
+    background-color: transparent;
+    border-top: inset rgb(190, 190, 190) 1px;
+    padding: 5px;
+  }
+
+  .circular-image{
+    border-radius: 100px;
+  }
+
+  .message-avatar{
+    height: 50px; width: 50px; margin: 0; background-size: 50px; border-radius: 100px; float: left; margin-right: 10px;
+  }
+
+  .message-content{
+    padding-left: 60px;
+  }
+
+  .character-title{
+    font-size: 11pt;
+    font-weight: bold;
+  }
+
+  .message-text{
+    font-size: 11pt;
+    margin-top: 5px;
+    white-space: pre-wrap; 
+    overflow-wrap: break-word;
+  }
 </style>
